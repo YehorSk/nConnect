0694aed91e984360f00b15733ca47f16537e8a2a
RomanVasylyk <131609378+RomanVasylyk@users.noreply.github.com>
2024-06-07 15:24:17 +0200

    scrolling

--- nConnect_Vue/src/router/index.js
+++ nConnect_Vue/src/router/index.js
@@ -208,7 +208,14 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
 });
 
 export default router;
